670b29c3db0632d99e6a9d786fbde34aa5a38383
Sam <bills2021e@gmail.com>
2022-09-06 08:29:44 -0400

    Add ESlint and additional test to cipher

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-restricted-syntax */
 export function capitalize(word) {
   const firstLetterUpper = word.charAt(0).toUpperCase();
   return word.replace(`${word[0]}`, `${firstLetterUpper}`);
 }
 
 export function reverseString(word) {
-  word = word.split("");
-  let reversedWord = [];
+  word = word.split('');
+  const reversedWord = [];
   for (let i = 0, j = word.length - 1; i < word.length; ++i, --j) {
     reversedWord[i] = word[j];
   }
-  return reversedWord.join("");
+  return reversedWord.join('');
 }
 
 export const calculator = (() => {
@@ -20,45 +25,47 @@ export const calculator = (() => {
   const multiply = (a, b) => a * b;
 
   const divide = (a, b) => {
-    let numberString = (a / b).toFixed(2);
+    const numberString = (a / b).toFixed(2);
     return b == 0 ? undefined : Number(`${numberString}`);
   };
 
-  return { add, subtract, multiply, divide };
+  return {
+    add, subtract, multiply, divide,
+  };
 })();
 
 export function caesarCipher(string, key) {
   if (key < 1) return string;
-  if (key > 26) key = key % 26;
+  if (key > 26) key %= 26;
   const cipherString = [];
-  string = string.split("");
+  string = string.split('');
   for (const letter of string) {
     const index = letter.charCodeAt(`${letter}`);
     if (index > 64 && index < 91) {
       index + key > 90
         ? (cipherString[cipherString.length] = String.fromCharCode(
-            index + key - 26
-          ))
+          index + key - 26,
+        ))
         : (cipherString[cipherString.length] = String.fromCharCode(
-            index + key
-          ));
+          index + key,
+        ));
     } else if (index > 96 && index < 123) {
       index + key > 122
         ? (cipherString[cipherString.length] = String.fromCharCode(
-            index + key - 26
-          ))
+          index + key - 26,
+        ))
         : (cipherString[cipherString.length] = String.fromCharCode(
-            index + key
-          ));
+          index + key,
+        ));
     } else {
       cipherString[cipherString.length] = letter;
     }
   }
-  return cipherString.join("");
+  return cipherString.join('');
 }
 
 export function analyzeArray(array) {
-  const length = array.length;
+  const { length } = array;
   let min = Number.POSITIVE_INFINITY;
   let max = Number.NEGATIVE_INFINITY;
   let average = 0;
@@ -67,5 +74,7 @@ export function analyzeArray(array) {
     if (number > max) max = number;
     if (number < min) min = number;
   }
-  return { length, min, max, average };
+  return {
+    length, min, max, average,
+  };
 }
